cb40657c842b704b7fe8d0178e2ff8b6bda50a88
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2023-10-30 20:56:25 -0400

    auto: Vault Backup (2023-10-30 20:56:25) - DESKTOP_MSI - Changed 2 Files
    Affected files:
    .vscode/settings.json
    03 - AREAS/Code/PowerShell/src/Mount-VirtualDrive.ps1

--- 03 - AREAS/Code/PowerShell/src/Mount-VirtualDrive.ps1
+++ 03 - AREAS/Code/PowerShell/src/Mount-VirtualDrive.ps1
@@ -6,32 +6,38 @@ Function Mount-VirtualDrive {
     Param(
         [Parameter(Mandatory=$True)]
         [ValidateScript({Test-Path -Path $_ -PathType File -Filter "*.vhd*"})]
-        [string]$Path,
-
-        [Parameter(Mandatory=$False)]
-        [string]$DriveLetter
+        [string]$Path
     )
 
-    Begin {
-        Set-StrictMode -Version Latest
+    $Success = 1
+
+
+    try { Mount-VHD -Path $Path -ErrorAction Stop }
+    catch {
+        Write-Verbose "Failed to mount $Path using Mount-VHD with PowerShell $($PSVersionTable.PSVersion)"
+        Write-Verbose "Attempting to mount $Path using DISKPART"
     }
 
-    Process {
-
-        try {
-            Mount-VHD -Path $Path -ErrorAction Stop
-        } catch {
-            Write-Warning "Failed to mount $Path using Mount-VHD with PowerShell $($PSVersionTable.PSVersion)"
-            Write-Verbose "Attempting to mount $Path using DISKPART"
-            try {
-                (("RESCAN`nSELECT VDISK FILE="$Path"`nATTACH VDISK`n") | DISKPART)
-            } catch {
-                Write-Warning "Failed to mount $Path using DISKPART"
-                throw $_
-            }
-        }
+    if ($?) {
+        Write-Verbose "Successfully mounted $Path"
+        $Success = 0
+    }
+    else {
+        Write-Verbose "Failed to mount $Path"
+        throw $_
     }
 
-    End {}
+    try { (("RESCAN`n`rSELECT VDISK FILE="$Path"`n`rATTACH VDISK`n`r") | DISKPART) -ErrorAction Stop }
+    catch { Write-Warning "Failed to mount $Path using DISKPART"; throw $_ }
 
+    if ($?) {
+        Write-Verbose "Successfully mounted $Path using DISKPART"
+        $Success = 0
+    }
+    else {
+        Write-Verbose "Failed to mount $Path using DISKPART"
+        throw $_
+    }
+
+    return $Success
 }
